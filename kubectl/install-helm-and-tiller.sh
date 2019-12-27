9e3f4991ab79faa7cfd895a5d15fc4321c65880a
Darin Pope <darin@planetpope.com>
2019-12-27 08:58:52 -0500

    add more echos

--- kubectl/install-helm-and-tiller.sh
+++ kubectl/install-helm-and-tiller.sh
@@ -2,6 +2,8 @@
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl --namespace kube-system create clusterrolebinding tiller-cluster-rule  --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
+echo ************************************
 echo sleeping for 15 seconds
+echo ************************************
 sleep 15
 helm version
